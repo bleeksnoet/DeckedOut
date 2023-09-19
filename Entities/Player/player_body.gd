a8cc3c9b2d7549d63b5d2e714ea9eaf6ad9351ed
bleeksnoet <bleachtgames@gmail.com>
2023-09-19 19:39:41 +0200

    update 2
    added template tiles, refined weapon system, prepared a jump button. now its waiting on sprites and animation ugh

--- Entities/Player/player_body.gd
+++ Entities/Player/player_body.gd
@@ -5,5 +5,8 @@ extends CharacterBody2D
 func _physics_process(delta):
 	var direction = Input.get_vector("Walk_West", "Walk_East", "Walk_North", "Walk_South")
 	velocity = direction * speed
-
+	
+	if Input.is_action_just_pressed("Jump"):
+		pass
+	
 	move_and_slide()
