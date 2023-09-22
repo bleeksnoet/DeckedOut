1a05eaf95f0689a873694231e320f8c29b032b14
bleeksnoet <bleachtgames@gmail.com>
2023-09-22 16:21:53 +0200

    State machine implementeed
    Statemachine made, made beginnings of a hurtbox

--- Entities/Player/player_body.gd
+++ Entities/Player/player_body.gd
@@ -12,31 +12,61 @@ extends CharacterBody2D
 var direction = Vector2.ZERO
 var speed = walkspeed
 
-func _physics_process(delta):
-	movement()
-	move_and_slide()
+enum states {
+	Idle,
+	Walking,
+	Falling
+}
+var CurrentState = states.Idle
 
-func movement():
+func _physics_process(delta):
 	direction.x = Input.get_action_strength("Walk_East") - Input.get_action_strength("Walk_West")
 	direction.y = Input.get_action_strength("Walk_South") - Input.get_action_strength("Walk_North")
 	direction = direction.normalized()
-	
-	if direction != Vector2.ZERO:
+	Walking()
+	Idle()
+	move_and_slide()
+	print(CurrentState)
+
+#can go to idle and jumping
+func Walking():
+	if CurrentState == states.Walking:
+		speed = walkspeed
 		animtree.set("parameters/Idle/blend_position", direction)
 		animtree.set("parameters/Walk/blend_position", direction)
 		animstate.travel("Walk")
 		velocity = direction * speed
-	else:
+
+#state changers
+		if direction == Vector2.ZERO:
+			CurrentState = states.Idle
+		if Input.is_action_just_pressed("Jump"):
+			Jumping()#Jumping is not a state because otherwise the speed wont change
+
+#Can go to walking and jumping
+func Idle():
+	if CurrentState == states.Idle:
 		animstate.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO,speedup)
-	
-	
-	if Input.is_action_just_pressed("Jump"):
-		Collison.disabled = true
-		speed = jumpspeed
-		animstate.travel("Jump")
+		
+#state changers
+		if direction != Vector2.ZERO:
+			CurrentState = states.Walking
+		if Input.is_action_just_pressed("Jump"):
+			Jumping() #Jumping is not a state because otherwise the speed wont change
+
+#At the end of jump returns to Idle
+func Jumping():
+	Collison.disabled = true
+	animstate.travel("Jump")
+	if !JumpTimer.time_left > 0:
 		JumpTimer.start()
 
 func _on_jump_timer_timeout():
 	speed = walkspeed
 	Collison.disabled = false
+	CurrentState = states.Idle
+
+
+func _on_dmg_checker_area_entered(area):
+	pass # Replace with function body.
