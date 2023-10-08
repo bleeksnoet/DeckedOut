5f829440cf01a9d547831d5b4585d4711507ff47
bleeksnoet <bleachtgames@gmail.com>
2023-10-09 00:16:19 +0200

    lots of changes
    fully removed jump, added sneaking. added shadows and vision. be sure to check vision example to see how it works

--- Entities/Player/player_body.gd
+++ Entities/Player/player_body.gd
@@ -1,23 +1,20 @@
 extends CharacterBody2D
 
-@export var walkspeed = 400  # speed in pixels/sec
+var mainspeed = 400
+@export var speed = 400  # speed in pixels/sec
 @export var speedup = 200
-@export var jumpspeed = 200
 @onready var animtree = $AnimationTree
 @onready var animplayer = $AnimationPlayer
-@onready var JumpTimer = $JumpTimer
 @onready var Collison = $Wallcol
 @onready var HPmanager = $HPmanager
-@onready var JumpCol = $JumpcolChecker/JumpCol
 @onready var HPBar = $HPBar
 @onready var animstate = animtree.get("parameters/playback")
 
 var direction = Vector2.ZERO
-var speed = walkspeed
 enum states {
 	Idle,
 	Walking,
-	Falling
+	Sneaking
 }
 var CurrentState = states.Idle
 
@@ -30,10 +27,10 @@ func _physics_process(delta):
 	direction = direction.normalized()
 	
 	Walking()
+	Sneaking()
 	Idle()
 	move_and_slide()
 
-#can go to idle and jumping
 func Walking():
 	if CurrentState == states.Walking:
 		animtree.set("parameters/Idle/blend_position", direction)
@@ -44,10 +41,17 @@ func Walking():
 #state changers
 		if direction == Vector2.ZERO:
 			CurrentState = states.Idle
-		if Input.is_action_just_pressed("Jump"):
-			Jumping()#Jumping is not a state because otherwise the speed wont change
 
-#Can go to walking and jumping
+func Sneaking():
+	if Input.is_action_pressed("Sneak"):
+		speed = speedup
+		$Sneaky/maincol.disabled = true
+		$Sneaky/Sneakcol.disabled = false
+	else:
+		speed = mainspeed
+		$Sneaky/maincol.disabled = false
+		$Sneaky/Sneakcol.disabled = true
+
 func Idle():
 	if CurrentState == states.Idle:
 		animstate.travel("Idle")
@@ -56,21 +60,6 @@ func Idle():
 #state changers
 		if direction != Vector2.ZERO:
 			CurrentState = states.Walking
-		if Input.is_action_just_pressed("Jump"):
-			Jumping() #Jumping is not a state because otherwise the speed wont change
-
-#At the end of jump returns to Idle
-func Jumping():
-	speed = jumpspeed
-	JumpCol.disabled = true
-	animstate.travel("Jump")
-	if !JumpTimer.time_left > 0:
-		JumpTimer.start()
-
-func _on_jump_timer_timeout():
-	speed = walkspeed
-	JumpCol.disabled = false
-	CurrentState = states.Idle
 
 func _on_hpmanager_died():
 	print("yes")
@@ -79,7 +68,6 @@ func _on_hpmanager_died():
 func _on_dmg_checker_body_entered(body):
 	HPBar.value = HPmanager.current_health
 	HPmanager.damage(1)
-
 
 func _on_jumpcol_checker_body_entered(body):
 	HPBar.value = HPmanager.current_health
