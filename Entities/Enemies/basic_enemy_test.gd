11fa95435aedcdb6de0256aac4a5f215eac4701b
bleeksnoet <bleachtgames@gmail.com>
2023-10-11 12:55:25 +0200

    particle test
    particle test to show where enemies are without actually seeing them. i consider it failed well try again later

--- Entities/Enemies/basic_enemy_test.gd
+++ Entities/Enemies/basic_enemy_test.gd
@@ -11,6 +11,7 @@ var target = null
 @onready var animstate = animtree.get("parameters/playback")
 @onready var DetectionCone = $Detectioncone
 @onready var nav = $NavigationAgent2D
+@onready var SoundTimer = $SoundTimer
 
 enum states{
 	Stationary,
@@ -24,10 +25,12 @@ func _ready():
 func _physics_process(delta):
 #	animtree.set("parameters/Idle/blend_position", velocity)
 	animtree.set("parameters/Walking/blend_position", velocity)
-	
+	print(SoundTimer.time_left)
 	var direction = Vector2()
 	
 	if currentstate == states.Chase:
+		if SoundTimer.time_left == 0:
+			SoundTimer.start()
 		LookTimer.stop()
 		nav.target_position = target.global_position
 		DetectionCone.look_at(target.global_position)
@@ -37,12 +40,16 @@ func _physics_process(delta):
 		direction = direction.normalized()
 	
 	if currentstate == states.Stationary:
+		SoundTimer.stop()
 		if LookTimer.time_left == 0:
 			LookTimer.start()
 		
 	velocity = direction * speed
 	
 	move_and_slide()
+
+func emit_sound():
+	$AnimatedSprite2D.play("Dust_Trail")
 
 func _on_detection_area_entered(area):
 	if RunTimer.time_left != 0:
@@ -69,13 +76,22 @@ func _on_look_timer_timeout():
 	DetectionCone.rotation_degrees += 90
 	if DetectionCone.rotation_degrees == 0:
 		animstate.travel("Idle_east")
+		emit_sound()
 	elif DetectionCone.rotation_degrees == 90:
 		animstate.travel("Idle_south")
+		emit_sound()
 	elif DetectionCone.rotation_degrees == 180:
 		animstate.travel("Idle_west")
+		emit_sound()
 	elif DetectionCone.rotation_degrees == 270:
 		animstate.travel("Idle_north")
+		emit_sound()
 	elif DetectionCone.rotation_degrees == 360:
 		animstate.travel("Idle_east")
+		emit_sound()
 		print("360 hit, returning to 0...")
 		DetectionCone.rotation_degrees = 0
+
+
+func _on_sound_timer_timeout():
+	emit_sound()
