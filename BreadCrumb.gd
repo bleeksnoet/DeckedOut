bf635f3f97b17fccb9aa4e4ee6480700bb6429b9
bleeksnoet <bleachtgames@gmail.com>
2023-12-06 17:12:55 +0100

    AI updates
    +started working on the modular AI system
    +some minor changes to the crumb system again. its done now i promise!

--- BreadCrumb.gd
+++ BreadCrumb.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@onready var animplayer = $AnimationPlayer
+
+func _ready():
+	if !animplayer.is_playing():
+		animplayer.play("Decay")
 #the bread has rotten!!
 func _on_lifetime_timeout():
 	queue_free()
--- NewAITest/ai_node.gd
+++ NewAITest/ai_node.gd
@@ -3,11 +3,25 @@ extends Node2D
 #all the nodes readied
 @onready var Wallchkr = $Wallchecker
 
+#health and damagio
+@export var damage = 5
+@export var HP = 5
+
+#finding out whoms'tve the target is
+var target = null
+
+#select your AI!
+enum Behaviourlist{none,chase} 
+#dropdownlist for the enum above
+@export var Behaviour = Behaviourlist.none
+
+
 #player entered the cone of sight! lets check if the AI can *actually* see them
 func _on_detection_area_area_entered(area):
 	Wallchkr.target_position = area.global_position - Wallchkr.global_position
+	if !Wallchkr.is_colliding():
+		print("not colliding!")
+		target = area
 
-
-#	Wallchkr.look_at(area.global_position)
-#	var size = Wallchkr.get_collision_point()
-#	print(Wallchkr.get_collision_point())
+func _physics_process(delta):
+	pass
